27c9bf34327c761a10d265c1f31675c0eb8a891b
Torveek <viktor.busarin@gmail.com>
2026-03-05 14:58:39 +0300

    REPO_URL fixed

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 DOTFILES_DIR="$HOME/.local/share/hypr-dotfiles"
-REPO_URL="https://github.com/Maciejonos/dotfiles.git"
+REPO_URL="https://github.com/Torveek/hypr-dotfiles.git"
 
 echo "=============="
 echo "Dotfiles Setup"
